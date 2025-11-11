8511afd8c5920833d97cd9444a037cd3fb241cf7
Roman Pavlyk <roman.p@developerdynamics.com>
2025-11-11 14:02:04 +0200

    [001]: deploy trigger

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
       <h1>Vite + React</h1>
       <h2>{import.meta.env.TEST_ENV_VARIABLE}</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count} $</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
